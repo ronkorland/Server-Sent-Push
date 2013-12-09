48fc191a74d15c0952629f32ee3a8db06df66fa1
atan <tanshiqi@gmail.com>
2013-12-09 15:00:56 +0800

    Just an example

---
+++ README.md
@@ -0,0 +1,7 @@
+Usage
+=====
+
+Send message in redis:
+  
+	127.0.0.1:6379>rpush message '{"title":"hello","message":"hello message","url":"http:\/\/www.google.com"}'
+
---
+++ push_source.php
@@ -0,0 +1,22 @@
+<?php 
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+header("Access-Control-Allow-Origin: *");
+
+
+
+
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379);
+
+echo "retry: 10000".PHP_EOL; // Reconnect every 10sec 
+$msg = $redis->lPop('message');
+if($msg){
+	echo "data: {$msg}".PHP_EOL; //Receive JSON Format 
+	echo PHP_EOL;
+}
+
+ob_flush();
+flush();
+
+
